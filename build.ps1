04b54dbd1482864e6cdabcc0795c76b85f8f208d
Stephen Gilboy <sgilboy@meredith.com>
2017-08-30 22:43:34 -0700

    Final Project

---
+++ build.ps1
@@ -0,0 +1,20 @@
+choco install visualstudio2017buildtools -y
+choco install visualstudio2017-workload-webbuildtools --package-parameters "--includeOptional" -y
+
+$msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\bin"
+$buildProperties = "/p:Configuration=Release;DeployOnBuild=true;PublishProfile=FolderProfile"
+
+
+if (Test-Path -Path msbuildPath -IsValid) {
+ $msbuild = Get-Item "$msbuildPath\MSBuild.exe"
+
+ # Publish Project
+ Write-Host  $publishProfile
+ cd "$PSScriptRoot\FuncAppDemo"
+ &$msbuild $publishProfile
+ cd $PSScriptRoot
+
+}
+else {
+ Write-Error -Message "MSBuild not found at location $msbuildPath"
+}
