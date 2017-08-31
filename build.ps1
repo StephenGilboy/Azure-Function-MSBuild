8c29290147a2b0d1fa81450d680c40d3e11907e6
Stephen Gilboy <sgilboy@meredith.com>
2017-08-30 22:55:24 -0700

    Add nuget

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 choco install visualstudio2017buildtools -y
 choco install visualstudio2017-workload-webbuildtools --package-parameters "--includeOptional" -y
+choco install nuget.commandline -y
 
 $msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\bin"
 $buildProperties = "/p:Configuration=Release;DeployOnBuild=true;PublishProfile=FolderProfile"
@@ -9,7 +10,7 @@ if (Test-Path -Path msbuildPath -IsValid) {
  $msbuild = Get-Item "$msbuildPath\MSBuild.exe"
 
  # Publish Project
- Write-Host  $publishProfile
+ nuget restore
  cd "$PSScriptRoot\FuncAppDemo"
  &$msbuild $publishProfile
  cd $PSScriptRoot
